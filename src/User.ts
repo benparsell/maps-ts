30eafcd0fe86b9ca5ce9cd1b2b57c69ea6e39527
Ben Parsell <benparsell@Bens-MacBook-Pro.local>
2022-09-29 22:52:57 -0400

    End of first section

--- src/User.ts
+++ src/User.ts
@@ -1,6 +1,8 @@
 import faker from 'faker';
+import { Mappable } from './CustomMap';
 
-class User {
+// By using implements, TS can tell us we are missing something
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
@@ -8,8 +10,18 @@ class User {
   }
 
   constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    }
+  }
 
+  markerContent() {
+    return `
+      <div>
+        <h1>User name: ${this.name}</h1>
+      </div>
+    `
   }
 }
-
-export default User;
